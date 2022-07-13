138c358879506a5f98b25b385973458abd45d86e
Crayon112 <karontjl112@gmail.com>
2022-07-13 17:27:17 +0800

    [refactor]规范化文档以及代码

--- web_search/user/user.py
+++ web_search/user/user.py
@@ -1,6 +1,6 @@
 # !usr/bin/env python
 # -*- encoding: utf-8 -*-
-"""用户类
+"""用户类.
 
 @File: user.py
 @Time: 2022/07/12 22:02:17
@@ -12,8 +12,10 @@
 
 
 class User(object):
+    """用户类."""
 
     def __init__(self, username, password, **kwargs) -> None:
+        """用户初始化."""
         self.username = username
         self.password = password
         self.kwargs = kwargs
